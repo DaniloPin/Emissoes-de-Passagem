1a4879185d1fc379ce73ae17ac9f2f22fe698284
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-23 16:57:40 -0300

    Add files via upload

---
+++ Passagem.cs
@@ -0,0 +1,9 @@
+﻿class Passagem {
+    public Passagem(string passageiro, string destino) {
+        Passageiro = passageiro;
+        Destino = destino;
+    }
+
+    public string Passageiro { get; set; }
+    public string Destino { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Passagem passagem = new Passagem("Danilo Pinheiro", "Suiça");
+
+
+Console.WriteLine($"Passajeito: {passagem.Passageiro}");
+Console.WriteLine($"Destino: {passagem.Destino}");
+
+
